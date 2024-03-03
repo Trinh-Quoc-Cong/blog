86d7a994b27a4dcf7a8aead64893c1f4dd3ffec9
Trinh-Quoc-Cong <cong.tq194495@sis.hust.edu.vn>
2024-03-03 21:07:53 +0700

    use bootrap

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const { engine } = require("express-handlebars");
+const app = express()
+const port = 3000
+
+
+// các file tĩnh bắt đầu từ đường dẫn public 
+// vd : localhost:3000/img/logo.jpeg
+app.use(express.static(path.join(__dirname,'public')))
+// HTTP logger
+app.use(morgan('combined'))
+
+// template engine
+app.engine("hbs", engine({
+    extname: '.hbs'
+}
+  
+));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+// router 
+
+
+//  127.0.0.1 - localhost
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
